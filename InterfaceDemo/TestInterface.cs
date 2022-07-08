d9a02d309fd3e0088b045967f3860cf41d97c9f8
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 17:57:48 +0530

    Add project files.

---
+++ InterfaceDemo/TestInterface.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    internal interface TestInterface
+    {
+        void Add(int a, int b);
+    }
+
+    internal interface TestInterface2 : TestInterface
+    {
+        void Sub(int a, int b);
+    }
+
+    internal class ClassTest : TestInterface2
+    {
+        void TestInterface.Add(int a, int b)
+        {
+            Console.WriteLine("Add = " + (a + b));
+        }
+
+        void TestInterface2.Sub(int a, int b)
+        {
+            Console.WriteLine("Sub = " + (a - b));
+        }
+
+        //public void Sub(int a, int b)
+        //{
+        //    Console.WriteLine("Sub = " + (a - b));
+        //}
+
+        //public void Add(int a, int b)
+        //{
+        //    Console.WriteLine("Add = " + (a + b));
+        //}
+
+        static void Main()
+        {
+            ClassTest c = new ClassTest();
+            //c.Add(20 , 20);
+            //c.Sub(100, 20);
+
+            TestInterface2 i = c;
+            i.Sub(10, 20);
+            i.Add(10, 20);
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+
+}
